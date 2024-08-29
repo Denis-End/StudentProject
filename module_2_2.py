21c74d87280563381fa970d1f9fcf76a35000b70
Denis-End <denisneotkda@gmail.com>
2024-08-29 12:30:18 +0400

    Add files via upload

--- module_2_2.py
+++ module_2_2.py
@@ -1,9 +1,10 @@
-frist=int(input('Введите число: '))
-second=int(input('Введите число: '))
-third=int(input('Введите число: '))
-if frist==second and frist==third:
+frist = int(input('Введите число: '))
+second = int(input('Введите число: '))
+third = int(input('Введите число: '))
+if frist == second and frist == third:
     print("3")
-elif second==frist:
+elif second == frist and second == third:
+    print("3")
+elif frist == second or second == third or third == frist:
     print("2")
-elif second!=frist and frist!=third:
-    print("0")
+else: print("0")
